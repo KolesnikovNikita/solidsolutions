8dc0d1eb00047fee192b03749f8e3e544aefc0cd
Nikita Kolesnikov <nik.kolesnikov.andrew@gmail.com>
2023-07-19 19:23:17 +0300

    js & style

--- js/index.js
+++ js/index.js
@@ -1,12 +1,15 @@
 function dropDownMenu() {
 	const btn = document.querySelector('.burger');
+	const burgerImg = document.querySelector('.burger-icon img');
 
 	btn.addEventListener('click', function () {
 		const menu = document.querySelector('.mobile-list');
 		if (menu.style.display == 'none') {
 			menu.style.display = 'block';
+			burgerImg.src = '../assets/images/burger-close.svg';
 		} else {
 			menu.style.display = 'none';
+			burgerImg.src = '../assets/images/burger-open.svg';
 		}
 	});
 }
