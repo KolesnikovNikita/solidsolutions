ec0ec65ce3d0d9718ced0054b52b9ceec7bea999
Nikita Kolesnikov <nik.kolesnikov.andrew@gmail.com>
2023-07-20 20:49:11 +0300

    20 july working

--- js/index.js
+++ js/index.js
@@ -14,8 +14,33 @@ function dropDownMenu() {
 	});
 }
 
+function initTabs() {
+	function switchTab(newIndex) {
+		tabPanels.forEach(function (tabPanel, index) {
+			tabPanel.hidden = !(index === newIndex);
+		});
+	}
+
+	const tabItems = document.querySelectorAll('.tab-item');
+	const tabPanels = document.querySelectorAll('.tab-content');
+
+	switchTab(0);
+
+	tabItems.forEach(function (tabItem, index) {
+		tabItem.addEventListener('click', function (event) {
+			tabItems.forEach(function (tab) {
+				tab.classList.remove('active-link');
+			});
+			event.preventDefault();
+			this.classList.add('active-link');
+			switchTab(index);
+		});
+	});
+}
+
 function initComponents() {
 	dropDownMenu();
+	initTabs();
 }
 
 initComponents();
