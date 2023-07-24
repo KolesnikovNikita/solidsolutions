be1afab8bda2fc55c8033e56bf58ebec21fd8eda
Nikita Kolesnikov <nik.kolesnikov.andrew@gmail.com>
2023-07-24 21:33:54 +0300

    swiper

--- js/index.js
+++ js/index.js
@@ -58,49 +58,48 @@ function initCheckbox() {
 
 // Slider
 
-function cardSlider() {
-	document.addEventListener('DOMContentLoaded', function () {
-		const slider = document.querySelector('.customer.slider');
-		const cardsContainer = slider.querySelector('.customer__cards');
-		const prevButton = slider.querySelector('.prev-button');
-		const nextButton = slider.querySelector('.next-button');
-		const cards = slider.querySelectorAll('.card');
-		const cardWidth = cards[0].offsetWidth; // Фиксированная ширина карточек (310px)
-		let currentCardIndex = 0;
+// function cardSlider() {
+// 	document.addEventListener('DOMContentLoaded', function () {
+// 		const slider = document.querySelector('.customer.slider');
+// 		const cardsContainer = slider.querySelector('.customer__cards');
+// 		const prevButton = slider.querySelector('.prev-button');
+// 		const nextButton = slider.querySelector('.next-button');
+// 		const cards = slider.querySelectorAll('.card');
+// 		const cardWidth = cards[0].offsetWidth; // Фиксированная ширина карточек (310px)
+// 		let currentCardIndex = 0;
 
-		function showCard(index) {
-			if (index >= 0 && index < cards.length) {
-				const offset = -cardWidth * index;
-				// Проверяем, чтобы карточка не выходила за границы видимой области
-				const maxOffset = -(cards.length - 1) * cardWidth;
-				cardsContainer.style.transform = `translateX(${Math.max(
-					offset,
-					maxOffset
-				)}px)`;
-				currentCardIndex = index;
-			}
-		}
+// 		function showCard(index) {
+// 			if (index >= 0 && index < cards.length) {
+// 				const offset = -cardWidth * index;
+// 				// Проверяем, чтобы карточка не выходила за границы видимой области
+// 				const maxOffset = -(cards.length - 1) * cardWidth;
+// 				cardsContainer.style.transform = `translateX(${Math.max(
+// 					offset,
+// 					maxOffset
+// 				)}px)`;
+// 				currentCardIndex = index;
+// 			}
+// 		}
 
-		function showPreviousCard() {
-			const newIndex = currentCardIndex - 1;
-			showCard(newIndex);
-		}
+// 		function showPreviousCard() {
+// 			const newIndex = currentCardIndex - 1;
+// 			showCard(newIndex);
+// 		}
 
-		function showNextCard() {
-			const newIndex = currentCardIndex + 1;
-			showCard(newIndex);
-		}
+// 		function showNextCard() {
+// 			const newIndex = currentCardIndex + 1;
+// 			showCard(newIndex);
+// 		}
 
-		prevButton.addEventListener('click', showPreviousCard);
-		nextButton.addEventListener('click', showNextCard);
-	});
-}
+// 		prevButton.addEventListener('click', showPreviousCard);
+// 		nextButton.addEventListener('click', showNextCard);
+// 	});
+// }
 
 function initComponents() {
 	dropDownMenu();
 	initTabs();
 	initCheckbox();
-	cardSlider();
 }
 
 initComponents();
