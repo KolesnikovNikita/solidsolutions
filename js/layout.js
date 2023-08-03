0312211cd15f953a2a93549a38bb09943fb547a0
Nikita Kolesnikov <nik.kolesnikov.andrew@gmail.com>
2023-08-03 11:45:49 +0300

    js files separated

--- js/layout.js
+++ js/layout.js
@@ -15,30 +15,6 @@ function dropDownMenu() {
 	});
 }
 
-function selectService() {
-	function switchTab(newIndex) {
-		tabPanels.forEach(function (tabPanel, index) {
-			tabPanel.hidden = !(index === newIndex);
-		});
-	}
-
-	const tabItems = document.querySelectorAll('.tab-item');
-	const tabPanels = document.querySelectorAll('.tab-content');
-
-	switchTab(0);
-
-	tabItems.forEach(function (tabItem, index) {
-		tabItem.addEventListener('click', function (event) {
-			tabItems.forEach(function (tab) {
-				tab.classList.remove('active-link');
-			});
-			event.preventDefault();
-			this.classList.add('active-link');
-			switchTab(index);
-		});
-	});
-}
-
 function activateLink() {
 	const links = document.querySelectorAll('.link-item');
 
@@ -88,26 +64,10 @@ function initCheckbox() {
 	});
 }
 
-function changeButtonText() {
-	const btn = document.getElementById('learnLink');
-	const windowWidth = 1200;
-	btn.innerText = 'Learn more';
-
-	if (window.innerWidth > windowWidth) {
-		btn.innerText = 'Learn More about us';
-	} else {
-		btn.innerText = 'Learn more';
-	}
-
-	window.addEventListener('resize', changeButtonText);
-}
-
-function initComponents() {
+function initFunctions() {
 	dropDownMenu();
-	selectService();
 	initCheckbox();
 	activateLink();
-	changeButtonText();
 }
 
-initComponents();
+initFunctions();
