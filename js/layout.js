8b41eca57e05368f8f418b1863848f753e554ad4
Mike <a1111@1111s-MacBook-Pro.local>
2023-08-11 08:43:23 +0300

    Fri 11 Aug case page for test

--- js/layout.js
+++ js/layout.js
@@ -1,42 +1,67 @@
 function dropDownMenu() {
-	const btn = document.querySelector('.burger');
-	const burgerImg = document.querySelector('.burger-icon img');
-	const header = document.querySelector('.page-header__navigation');
+  const btn = document.querySelector(".burger");
+  const burgerImg = document.querySelector(".burger-icon img");
+  const header = document.querySelector(".page-header__navigation");
 
-	btn.addEventListener('click', function () {
-		const menu = document.querySelector('.mobile-list');
-		if (!header.classList.contains('active-header')) {
-			header.classList.add('active-header');
-			burgerImg.src = '../assets/images/burger-close.svg';
-		} else {
-			header.classList.remove('active-header');
-			burgerImg.src = '../assets/images/burger-open.svg';
-		}
-	});
+  btn.addEventListener("click", function () {
+    const menu = document.querySelector(".mobile-list");
+    if (!header.classList.contains("active-header")) {
+      header.classList.add("active-header");
+      burgerImg.src = "../assets/images/burger-close.svg";
+    } else {
+      header.classList.remove("active-header");
+      burgerImg.src = "../assets/images/burger-open.svg";
+    }
+  });
 }
 
 function activateLink() {
-	const links = document.querySelectorAll('.link-item');
+  const links = document.querySelectorAll(".link-item");
 
-	function chooseLink(linkElement) {
-		links.forEach(link => {
-			link.classList.remove('active-item');
-		});
-		linkElement.classList.add('active-item');
-	}
+  function chooseLink(linkElement) {
+    links.forEach((link) => {
+      link.classList.remove("active-item");
+    });
+    linkElement.classList.add("active-item");
+  }
 
-	links.forEach(link => {
-		link.addEventListener('click', function () {
-			chooseLink(this);
-		});
-	});
+  links.forEach((link) => {
+    link.addEventListener("click", function () {
+      chooseLink(this);
+    });
+  });
 
-	chooseLink(links[0]);
+  chooseLink(links[0]);
+}
+
+function scrollUp() {
+  const scrollUp = document.querySelector(".scroll-up");
+  const offset = 100;
+
+  function getTop() {
+    return window.scrollY || document.documentElement.scrollTop;
+  }
+
+  window.addEventListener("scroll", function () {
+    if (getTop() > offset) {
+      scrollUp.classList.add("scroll-up-active");
+    } else {
+      scrollUp.classList.remove("scroll-up-active");
+    }
+  });
+
+  scrollUp.addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
 }
 
 function initFunctions() {
-	dropDownMenu();
-	activateLink();
+  dropDownMenu();
+  activateLink();
+  scrollUp();
 }
 
 initFunctions();
--- js/swiper.js
+++ js/swiper.js
@@ -1,27 +1,51 @@
 function updateSlidesPerView() {
-	var windowWidth = window.innerWidth;
-	var slidesPerView = 1;
+  var windowWidth = window.innerWidth;
+  var slidesPerView = 1;
 
-	if (windowWidth >= 1024) {
-		slidesPerView = 2;
-	}
+  if (windowWidth >= 1024) {
+    slidesPerView = 2;
+  }
 
-	swiper.params.slidesPerView = slidesPerView;
-	swiper.update();
+  swiper.params.slidesPerView = slidesPerView;
 }
 
-const swiper = new Swiper('.swiper', {
-	direction: 'horizontal',
-	loop: true,
+function updateSlidesCasesPerView() {
+  var windowWidth = window.innerWidth;
+  var slidesPerView = 1;
 
-	slidesPerView: 1,
-	slidesPerGroup: 1,
+  if (windowWidth >= 1024) {
+    slidesPerView = 2;
+  }
 
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
-	},
+  swiperCase.params.slidesPerView = slidesPerView;
+}
+
+const swiper = new Swiper(".swiper", {
+  direction: "horizontal",
+  loop: true,
+
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
 
-window.addEventListener('resize', updateSlidesPerView);
+const swiperCase = new Swiper(".swiper-of-cases", {
+  direction: "horizontal",
+  loop: true,
+
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+window.addEventListener("resize", updateSlidesPerView);
 updateSlidesPerView();
+updateSlidesCasesPerView();
