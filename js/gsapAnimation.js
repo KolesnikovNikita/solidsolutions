b9132a929797bf9dce26125a1046e001eaceeb8e
Mike <a1111@1111s-MacBook-Pro.local>
2023-11-17 14:28:49 +0200

    Remove .DS_Store files from index

--- js/gsapAnimation.js
+++ js/gsapAnimation.js
@@ -1,25 +1,37 @@
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.from('.cards-list__item', {
+// gsap.from('.cards-list__item', {
+//     scrollTrigger: {
+//         trigger: '.head-content',
+
+//         start: '-500px top',
+//     },
+//     x: -100,
+//     opacity: 0,
+//     duration: 0.4,
+//     stagger: 0.2,
+// });
+
+// gsap.from('.tech-list', {
+//     scrollTrigger: {
+//         trigger: '.services-page__technologies',
+//         markers: true,
+//         start: '-500px top',
+//     },
+//     y: -100,
+//     opacity: 0,
+//     duration: 0.8,
+//     stagger: 0.4,
+// });
+
+gsap.from('.chat-image__1', {
     scrollTrigger: {
-        trigger: '.head-content',
-
-        start: '-500px top',
-    },
-    x: -100,
-    opacity: 0,
-    duration: 0.4,
-    stagger: 0.2,
-});
-
-gsap.from('.tech-list', {
-    scrollTrigger: {
-        trigger: '.services-page__technologies',
-
-        start: '-500px top',
+        trigger: '.technologies-used',
+        start: 'top center',
     },
     y: -100,
-    opacity: 0,
+    autoAlpha: 0,
+    once: true,
     duration: 0.8,
-    stagger: 0.4,
+    scale: 1.1,
 });
