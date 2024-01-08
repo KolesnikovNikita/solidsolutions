4390882f2dcbb8f9dd5925c347f7b5274f78034b
Mike <a1111@1111s-MacBook-Pro.local>
2024-01-08 15:39:07 +0200

    Mon 8 adding new pages

--- js/swiper.js
+++ js/swiper.js
@@ -1,51 +1,51 @@
 function updateSlidesPerView() {
-  var windowWidth = window.innerWidth;
-  var slidesPerView = 1;
+    var windowWidth = window.innerWidth;
+    var slidesPerView = 1;
 
-  if (windowWidth >= 768) {
-    slidesPerView = 2;
-  }
+    if (windowWidth >= 768) {
+        slidesPerView = 3;
+    }
 
-  swiper.params.slidesPerView = slidesPerView;
+    swiper.params.slidesPerView = slidesPerView;
 }
 
 function updateSlidesCasesPerView() {
-  var windowWidth = window.innerWidth;
-  var slidesPerView = 1;
+    var windowWidth = window.innerWidth;
+    var slidesPerView = 1;
 
-  if (windowWidth >= 1024) {
-    slidesPerView = 2;
-  }
+    if (windowWidth >= 1024) {
+        slidesPerView = 2;
+    }
 
-  swiperCase.params.slidesPerView = slidesPerView;
+    swiperCase.params.slidesPerView = slidesPerView;
 }
 
-const swiper = new Swiper(".swiper", {
-  direction: "horizontal",
-  loop: true,
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
 
-  slidesPerView: 1,
-  slidesPerGroup: 1,
+    slidesPerView: 1,
+    slidesPerGroup: 1,
 
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
 });
 
-const swiperCase = new Swiper(".swiper-of-cases", {
-  direction: "horizontal",
-  loop: true,
+const swiperCase = new Swiper('.swiper-of-cases', {
+    direction: 'horizontal',
+    loop: true,
 
-  slidesPerView: 1,
-  slidesPerGroup: 1,
+    slidesPerView: 1,
+    slidesPerGroup: 1,
 
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
 });
 
-window.addEventListener("resize", updateSlidesPerView);
+window.addEventListener('resize', updateSlidesPerView);
 updateSlidesPerView();
 updateSlidesCasesPerView();
